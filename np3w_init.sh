560bb9ddebe07701719f9c5fe3759f036d821679
Np3w <np3w.np3w@gmail.com>
2016-11-11 17:17:02 +0100

    Update np3w_init.sh

--- np3w_init.sh
+++ np3w_init.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 # Np3w's initialization script
 
-xset r rate 160 30&
+# Seems to fail more often without the sleep
+sleep 10 
+
 unclutter -idle 5&
+ktimer&
+redshift &
+
+# remap button right of left shift on nordic keyboards to escape
+xmodmap -e "keycode 94 = Escape"
+# increase keyboard repeat rate
+xset r rate 160 30
