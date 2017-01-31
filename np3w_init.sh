21271873134970a4acdf85d5b72c64e47cbe998c
Np3w <np3w.np3w@gmail.com>
2017-01-31 19:08:46 +0100

    Update np3w_init.sh

--- np3w_init.sh
+++ np3w_init.sh
@@ -1,12 +1,36 @@
 #!/bin/bash
 # Np3w's initialization script
 
-# Seems to fail more often without the sleep
-sleep 10 
+####
+# Read command line arguments
+XRECOVER=false
+while [[ $# -gt 0 ]]
+do
+	case "$1" in --xrecover)
+	XRECOVER=true
+	shift
+	;;
+	*)
+	# unknown option
+	echo "Unknown argument $1"
+	exit 1
+	;;
+	esac
+done
 
-unclutter -idle 5&
-ktimer&
-redshift &
+####
+# Actually do stuff
+
+# Seems to fail sometimes without the sleep
+sleep 3
+
+if [ $XRECOVER = false ]; then
+	#Start programs
+	unclutter -idle 5&
+	#ktimer&
+	redshift -t 5500K:2000K &
+	nemo -n
+fi
 
 # remap button right of left shift on nordic keyboards to escape
 xmodmap -e "keycode 94 = Escape"
