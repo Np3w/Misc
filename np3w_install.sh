bfe5fda9e3e20f089489abbfcd5da3d24405aec5
Np3w <np3w.np3w@gmail.com>
2017-08-04 20:10:08 +0200

    4coder start script

--- np3w_install.sh
+++ np3w_install.sh
@@ -1,20 +1,42 @@
 #!/bin/bash
 
 # Script that sets up a nice linux environment
-ln -sf ~/proj/Misc/.vimrc ~/.vimrc
 
+echo "Installing vim/neovim configuration"
+ln -sf ~/proj/Misc/.vimrc ~/.vimrc
 mkdir -p ~/.config/nvim
 ln -sf ~/proj/Misc/.vimrc ~/.config/nvim/init.vim
 
+echo "Installing shell configuration files"
 ln -sf ~/proj/Misc/.zshrc ~/.zshrc
 ln -sf ~/proj/Misc/.screenrc ~/.screenrc
 
+echo "Installing 20_np_suspend script"
 sudo ln -sf ~/proj/Misc/20_np_suspend /lib/systemd/system-sleep/20_np_suspend
 
+echo "Setting up i3wm configuration"
 mkdir -p ~/.config/i3
 ln -sf ~/proj/Misc/i3_config ~/.config/i3/config
 
+echo "Setting up git"
 git config --global user.email np3w.np3w@gmail.com
 git config --global user.name Np3w
 git config --global push.default simple
+
+echo "Installing 4coder starter program"
+sudo ln -sf ~/proj/Misc/4ed /usr/bin/4ed
+
+# Download tools and libraries
+if [ ! -d ~/proj/np_libs ] ; then
+	git clone git@github.com:Np3w/Libs.git ~/proj/np_libs
+else
+	echo "Not downloading np_libs because they already exist"
+fi
+
+echo "Installing np tools"
+pushd ~/proj/np_libs
+	make
+	sudo make install_tools
+popd
+
 
