49c4001c892c83f46bc812b4831e5d187a430459
Np3w <np3w.np3w@gmail.com>
2017-12-28 22:43:45 +0100

    Update np3w_install.sh

--- np3w_install.sh
+++ np3w_install.sh
@@ -2,6 +2,28 @@
 
 # Script that sets up a nice linux environment
 
+# Install packages using apt
+INSTALL_PACKAGES=1
+
+if [[ $INSTALL_PACKAGES -eq 1 ]] ; then
+	echo "Installing packages"
+	sudo apt install git
+	sudo apt install subversion
+
+	sudo apt install gcc
+	sudo apt install clang
+	sudo apt install build-essentials
+
+	sudo apt install cgdb
+	sudo apt install gdb
+
+	sudo apt install i3-wm
+
+	sudo apt install ssh
+
+	sudo apt install xclip
+fi
+
 echo "Installing vim config"
 ln -sf ~/proj/Misc/.vimrc ~/.vimrc
 
@@ -38,17 +60,27 @@ else
 	echo "4coder not installed in \"$FCODER_DIR\". Not installing 4coder configuration"
 fi
 
+HAVE_NP_LIBS=0
+
 # Download tools and libraries
 if [ ! -d ~/proj/np_libs ] ; then
 	git clone git@github.com:Np3w/Libs.git ~/proj/np_libs
+	if [[ $? -eq 0 ]] ; then
+		HAVE_NP_LIBS=1
+	else
+		echo "Failed to download np_libs"
+	fi
 else
 	echo "Not downloading np_libs because they already exist"
+	HAVE_NP_LIBS=1
 fi
 
-echo "Installing np tools"
-pushd ~/proj/np_libs
-	make
-	sudo make install_tools
-popd
+if [[ $HAVE_NP_LIBS -eq 1 ]] ; then
+	echo "Installing np tools"
+	pushd ~/proj/np_libs
+		make
+		sudo make install_tools
+	popd
+fi
 
 
